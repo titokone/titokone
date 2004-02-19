e0ada7560fc0988526d081f45aca4ced6c5a12ed
Antti Takalahti <takalaht@cs.helsinki.fi>
2004-02-19 19:54:06 +0000

    methods

--- Loader.java
+++ Loader.java
@@ -1,6 +1,14 @@
 /** This class can load a TTK91Application. It changes the processor state
     accordingly. */
 public class Loader { 
-    // (needs the Processor as a parameter?)
+
+/**You can set the file to load.
+*/
+public void setFileToLoad(Application application){}
+
+/**Loads a single line to memory.
+	@return Info from the loaded line.
+*/
+public LoadInfo loadALine(){} 
 
 }
--- Settings.java
+++ Settings.java
@@ -1,3 +1,64 @@
 /** This class keeps track of the settings. It can parse and save settings
     files. */
-public class Settings { }
+public class Settings { 
+
+	/**You can set the language of user interface. 
+	*/
+public void setLanguage(String newLanguage){}
+
+	/**
+            @return Current language of the user interface.
+        */
+public String getLanguage(){}
+
+
+	/**You can set the running mode (one command at a time, with comments etc.)
+	*/
+public void setRunMode(String newMode){}
+
+	/** Returns the current language.
+            @return Current language.
+        */
+public String getRunMode(){}
+
+
+	/**You can set the file that the simulator uses to emulate disc input with.
+	*/
+public void setInputFile(String newInputFileName){}
+
+	/** 
+            @return Name of the file that emulates disc input.
+        */
+public String getInputFile(){}
+
+
+	/**You can set the file that the simulator uses to emulate disc output with.
+	*/
+public void setOutputFile(String newOutputFileName){}
+
+	/**	
+	    @return Name of the file that emulates disc output.
+	*/
+public String getOutputFile(){}
+
+
+	/**You can set the file where the settings are saved to.
+        */
+public void setSettingsFile(String newSettingsFile){}
+
+	/** 
+            @return The name of the file where settings are saved to.
+        */
+public String getSettingsFile(){}
+
+
+	/**You can set the new size for the memory.
+	*/
+public void setMemorysize(int newMemorySize){}
+
+	/** 
+            @return Current memory size.
+        */
+public int getMemorysize(){}
+
+}
