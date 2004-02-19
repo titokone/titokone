1591561eb58e0d6df128fa7bf294c6c84ee3823b
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-02-19 18:49:07 +0000

    Some more basis for CompileConstants.

--- CompileConstants.java
+++ CompileConstants.java
@@ -5,6 +5,14 @@ public class CompileConstants {
     private HashTable opcodes;
     private HashTable commands;
 
+    private static final Object[][] data = {
+	{"NOP", new Integer(0)},
+	{"STORE", new Integer(1)} // etc.
+    }
+
+    public CompileConstants() { // Set up the hashtables.
+    }
+
     public String getCommand(int opcode) {  }
 
     public int getOpcode(String command) { }
