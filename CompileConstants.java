471bb1553e9253c6789c997c7b3bc34cf15cf759
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-02-19 18:46:16 +0000

    Got Compiler basically done, but it needs some tweaking yet. Created the basic CompileConstants class.

---
+++ CompileConstants.java
@@ -0,0 +1,11 @@
+/** This class contains the bulk data needed to translate commands to 
+    opcodes and vice versa. 
+    TODO: Anything else needed? */
+public class CompileConstants {
+    private HashTable opcodes;
+    private HashTable commands;
+
+    public String getCommand(int opcode) {  }
+
+    public int getOpcode(String command) { }
+}
--- Compiler.java
+++ Compiler.java
@@ -1,18 +1,118 @@
 /** This class knows everything about the relation between symbolic
     code and binary code. It can transform a full source to binary or
-    one symbolic command to binary or vice versa. */
+    one symbolic command to binary or vice versa. 
+    TODO: Koksi empties out all compiler commands and empty lines at the start
+    of round 2, maybe we ought to do that too. */
 public class Compiler {
-    // Problem: translating back to symbolic from binary will lose 
-    // any symbol data, eg. LOAD R1, X compiled to binary and back will 
-    // become LOAD R1, 100 if X=100. The effects of this can be avoided
-    // in the GUI by getting the symbol data directly from source. 
-    
-    /** This function transforms a symbolic source code into an 
-	application class. 
-	@param source The symbolic source code to be compiled.
-	@return The compiled application. */
-    public Application compile(CompileSource source) { }
+    /** This field contains the source code as a StringTokenizer, delimited by 
+	\n\r\f. */
+    private StringTokenizer source;
+    /** This field contains the symbol table in its incomplete form. Some of 
+	the values corresponding to the keys may be null, if they have not
+	been defined yet. */
+    private HashTable symbols;
 
-    // - get binary from symbolic command
-    // - get symbolic command from binary
+    /** This field keeps track of whether we are in the first round of compilation
+	or the second. It is set by compile() and updated by compileLine(). */
+    private boolean firstRound;
+
+    /** This field counts the number of actual command lines found during the 
+	first round. */
+    private int commandLineCount;
+
+    /** This field keeps track of the size of the data area expanded during the
+	first round. */
+    private int dataAreaSize;
+    
+    /** This array contains the code. It will not be touched until the second 
+	round of compilation. */
+    private MemoryLine[] code;
+
+    /** This array contains the data. It will not be touched until the second 
+	round of compilation. */
+    private MemoryLine[] data;
+
+    /** This field contains the debugger instance to inform of any compilation
+	happenings. */
+    private Debugger debugger;
+
+    /** This constructor sets up the class. It also initializes an instance of 
+	CompileConstants and Debugger. */
+    public Compiler() { }
+
+    /** This function initializes transforms a symbolic source code into an 
+	application class. After this, call compileLine() to actually compile
+	the application one line at a time, and finally getApplication() to get
+	the finished application. 
+	@param source The symbolic source code to be compiled. */
+    public void compile(CompileSource source) { }
+
+    /** This function goes through one line of the code. On the first round, it
+	gathers the symbols and their definitions to a symbol table and conducts 
+	syntax-checking, on the second round it transforms each command to its 
+	binary format. For the transformations, the CompileConstants class is 
+	used. It calls the private methods firstRoundProcess() and 
+	secondRoundProcess() to do the actual work, if there is any to do.
+	The transfer from first round of compilation to the second is done 
+	automatically; during it, initializeSecondRound() is called.
+	@return A CompileInfo debug information object, describing what happened
+	during the compilation of this line and whether this is the first or second
+	round of compilation or null if there are no more lines left to process.
+	@throws TTK91CompileException If a) there is a syntax error during the first
+	round of checking (error code 101) or b) a symbol is still undefined after 
+	the first round of compilation is finished. */
+    public CompileInfo compileLine() throws TTK91CompileException {  }
+
+    /** This method returns the readily-compiled application if the compilation
+	is complete, or null otherwise. */
+    public Application getApplication() { }
+
+    /** This function transforms a binary command number to a MemoryLine 
+	containing both the binary and the symbolic command corresponding 
+	to it.
+	@param binary The command to be translated as binary.
+	@return A MemoryLine instance containing both the information 
+	about the symbolic command and the binary version of it. */
+    public MemoryLine getSymbolicAndBinary(int binary) {  }
+
+    /** This function transforms a MemoryLine containing only the binary 
+	command to a MemoryLine containing both the binary and the 
+	symbolic command corresponding to it.
+	@param binaryOnly A MemoryLine containing the binary only of the 
+	command to be translated as binary. If the MemoryLine contains
+	both, the pre-set symbolic value is ignored.
+	@return A MemoryLine instance containing both the information 
+	about the symbolic command and the binary version of it. */
+    public MemoryLine getSymbolicAndBinary(MemoryLine binaryOnly) {  }
+
+    /** This function gathers new symbol information from the given line 
+	and checks its syntax. If a data reservation is detected, the 
+	dataAreaSize is incremented accordingly. If the line contains 
+	an actual command, commandLineCount is incremented. 
+	@param line The line of code to process. 
+	@return A CompileInfo object describing what was done, or null if 
+	the first round has been completed. This will be the sign for
+	the compiler to prepare for the second round and start it. */
+    private CompileInfo firstRoundProcess(String line) { }
+
+    /** This method initializes the code and data area arrays for the
+	second round processing according to the dataAreaSize and 
+	commandLineCount variables. It also resets the StringTokenizer by
+	calling tokenize(). */
+    private void initializeSecondRound() { }
+
+    /** This function transforms any commands to binary and stores both
+	forms in the code array, or sets any initial data values in the
+	data array. It uses CompileConstants as its aid. 
+	@param line The line of code to process.
+	@return A CompileInfo object describing what was done, or 
+	null if the second round and thus the compilation has been 
+	completed. */
+    private CompileInfo secondRoundProcess(String line) {  }
+
+    /** This function generates a StringTokenizer from the source string. 
+	The delimiters used are \r\n\f.
+	@param source The source string to tokenize.
+	@return A StringTokenizer containing the source. */
+    private StringTokenizer tokenize(String source) { }
 }
