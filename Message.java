ad8585cf6c1de050cdb7876d76e0f94e8f4dc67a
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-02-23 19:32:03 +0000

    Runkorunko.

---
+++ Message.java
@@ -0,0 +1,9 @@
+/** This class represents a translatable message to the user. */
+public class Message {
+  // different constructors: plain message, message with one parameter,
+  // message with multiple parameters. (That is, replaceables.)
+
+  /** This method translates the message and does any necessary
+      replacement of parameters into the string itself. */
+  public String toString() {}
+}
