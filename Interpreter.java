76b96c1dfb9461d0c1b6dbad725fad0c585ce52a
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-02-27 13:51:12 +0000

    Interpreterin runko täydennettäväksi. Ehkä joku hashtaulujen luontimetodi olisi myös kiva.

---
+++ Interpreter.java
@@ -0,0 +1,26 @@
+/** This class contains information common to various interpreters. */
+public class Interpreter {
+  /** This field contains a two-dimensional array of translations 
+      between opcodes as integers and as symbolic command names. 
+      The command names are in all capital letters. */
+  protected static final Object[][] commandData = {
+    {"NOP", new Integer(0)},
+    {"STORE", new Integer(1)} // etc.
+  }
+  
+  /** This field contains a two-dimensional array of translations
+      between memory addressing types as integers and as symbols
+      repressenting them. */
+  protected static final Object[][] addressingData = {
+    {"", new Integer(0) }, // TARKISTA ARVOT!
+    {"=", new Integer(1) },
+    {"@", new Integer(2) }
+  }
+
+  /** This field contains a two-dimensional array of translations
+      between register symbolic names and the integers used to 
+      represent them in numeric commands. */
+  protected static final Object[][] registerData = {
+    {"R0", new Integer(0) } // etc; tarkista arvot!
+  }
+}
