2e89034dfb019ad21eb0338ee4a215a68b65b242
Sampo Yrjänäinen <szyrjana@cs.helsinki.fi>
2004-02-29 17:02:34 +0000

    taulukot täytetty

--- Interpreter.java
+++ Interpreter.java
@@ -11,22 +11,65 @@ public class Interpreter {
 	The command names are in all capital letters. */
     protected static final Object[][] commandData = {
 	{"NOP", new Integer(0)},
-	{"STORE", new Integer(1)} // etc.
+        {"STORE", new Integer(1)},
+        {"LOAD", new Integer(2)},
+        {"IN", new Integer(3)},
+        {"OUT", new Integer(4)},
+        {"ADD", new Integer(17)},
+        {"SUB", new Integer(18)},
+        {"MUL", new Integer(19)},
+        {"DIV", new Integer(20)},
+        {"MOD", new Integer(21)},
+        {"AND", new Integer(22)},
+        {"OR", new Integer(23)},
+        {"XOR", new Integer(24)},
+        {"SHL", new Integer(25)},
+        {"SHR", new Integer(26)},
+        {"COMP", new Integer(31)},
+        {"JUMP", new Integer(32)},
+        {"JNEG", new Integer(33)},
+        {"JZER", new Integer(34)},
+        {"JPOS", new Integer(35)},
+        {"JNNEG", new Integer(36)},
+        {"JNZER", new Integer(37)},
+        {"JNPOS", new Integer(38)},
+        {"JLES", new Integer(39)},
+        {"JEQU", new Integer(40)},
+        {"JGRE", new Integer(41)},
+        {"JNLES", new Integer(42)},
+        {"JNEQU", new Integer(43)},
+        {"JNGRE", new Integer(44)},
+        {"CALL", new Integer(49)},
+        {"EXIT", new Integer(50)},
+        {"PUSH", new Integer(51)},
+        {"POP",  new Integer(52)},
+        {"PUSHR", new Integer(53)},
+        {"POPR",  new Integer(54)},
+        {"SVC", new Integer(112)}
     };
     
     /** This field contains a two-dimensional array of translations
 	between memory addressing types as integers and as symbols
 	repressenting them. */
     protected static final Object[][] addressingData = {
-	{"", new Integer(0) }, // TARKISTA ARVOT!
-	{"=", new Integer(1) },
-	{"@", new Integer(2) }
+        {"=", new Integer(0)},
+        {"", new Integer(1)},
+        {"@", new Integer(2)}
     };
     
     /** This field contains a two-dimensional array of translations
 	between register symbolic names and the integers used to 
 	represent them in numeric commands. */
     protected static final Object[][] registerData = {
-	{"R0", new Integer(0) } // etc; tarkista arvot!
+        {"R0", new Integer(0)},
+        {"R1", new Integer(1)},
+        {"R2", new Integer(2)},
+        {"R3", new Integer(3)},
+        {"R4", new Integer(4)},
+        {"R5", new Integer(5)},
+        {"R6", new Integer(6)},
+        {"R7", new Integer(7)},
+        {"SP", new Integer(6)},
+        {"FP", new Integer(7)},
     };
 }
