b49d5d38669b5b3b4ef700e7f3e2e79dddea8fd9
Seppo Hätönen <shatonen@cs.helsinki.fi>
2004-03-08 21:39:59 +0000

    Melkein valmis, javadoc lähes valmis, hieman muita ongelmia

--- CompileInfo.java
+++ CompileInfo.java
@@ -1,4 +1,4 @@
-/** This class is used to tell GUIBrain what compiler has done at the moment.
+ /** This class is used to tell GUIBrain what compiler has done at the moment.
     CompileDebugger creates objects from this class and passes them to the 
     GUIBrain.
 */
@@ -67,10 +67,9 @@ public class CompileInfo extends DebugInfo {
   private boolean definingDC;
   private int value; // DC/DS.
 
-  public CompileInfo(String statusMessage, short phase, int lineNumber,
-		     String lineContents) {}
+  public CompileInfo(short phase, int lineNumber, String lineContents) {}
   // (final phase constructor)
-  public CompileInfo(String statusMessage, short phase, int lineNumber) {}
+  public CompileInfo(short phase, int lineNumber) {}
     
   /* Vielä metodit tueksi näille:
      all: set statusmessage (in parent), set phase, set linenumber
@@ -89,33 +88,135 @@ public class CompileInfo extends DebugInfo {
     
     /** This method sets lineEmpty value to true. */
     public void setLineEmpty(){}
+
+    /** This method sets the statusMessage.
+	@param statusMessage String containing the message.
+    */
+    public void setStatusMessage(String statusMessage){}
+    
+    /** This method sets the comments going to the GUI.
+	@param comment String containing the comments.
+    */
     public void setComments(String comment){}
+
+    /** This field sets the name of a found symbol.
+	@param name String containing the symbol name.
+    */
     public void setSymbolName(String name){}
+    
+    /** This method sets the name of a found symbol and its value.
+	@param name Name of the symbol.
+	@param value Value of the symbol.
+    */
     public void setSymbolName(String name, int value){}
+ 
+    /** This method sets the name of a found label and sets the labelDefined 
+	field to true.
+	@param name Name of the label.
+    */
     public void setLabelName(String name){}
+    
+    /** This method tells CompileInfo that a given label was defined and it's
+	value. It sets labelDefined field to true.
+	@param name String containing the name of the label.
+	@param value An integer value where the label points.
+    */
+    public void setLabelDefined(String name, int value){}
+    
+    /** This method tells what integer represents the compiled line of the
+	code.
+	@param binary An integer value representing the symbolic command.
+    */
     public void setLineBinary(int binary){}
-    public void setLabelDefined(String name, boolean defined){}
-    public void setLabelValue(String name, int value){}
+
+
     public void setDefiningDS(int ADDR, int size){}
+    /** This method sets the boolean field finalFinal to true.
+     */
     public void setFinal(){}
+    /** This method sets the initial SP and FP pointers to given values.
+	@param SP Initial value of SP.
+	@param FP Initial value of FP.
+    */
     public void setInitPointers(int SP, int FP){}
+
+    
     public void setMemoryline(int lineNumber, int value){}
 
+    /** This method returns the statusmessage.
+	@return String containing the message.
+    */
     public String returnStatusMessage(){}
+ 
+    /** This method returns comments made by CompileDebugger.
+	@return String containing the comments.
+    */
     public String returnComments(){}
+    
+    /** This message tells that an empty line or line containing only 
+	whitespaces was compiled.
+    */
     public boolean returnLineEmpty(){}
+    
+    /** This method returns current phase as a short. 0 for first round, 1 for
+	second and 2 for final.
+	@return Short containing phase.
+    */
     public short returnPhase(){}
+
+    /** This method return symbolic contents of the line.
+	@return String containing comments.
+    */
     public String returnLineContents(){}
+
+    /** This method returns found symbolname.
+	@return String containing the name.
+    */
     public String returnSymbolName(){}
+
+    /** This method returns true if symbol was defined.
+	@return boolean containing information if symbol was defined.
+    */
     public boolean returnSymbolDefined(){}
+
+    /** This method returns value of current symbol.
+	@return An integer containing symbol's value.
+    */
     public int returnSymbolValue(){}
+
+    /** This method returns compiled binary machinecommand represented as an
+	integer value.
+	@return An integer representing machine command.
+    */
     public int returnLineBinary(){}
+
+    /** This method returns the name of the current label.
+	@param String containing name of the label.
+    */
     public String returnLabelName(){}
-    public int returnLabelValue(){} // tarkasta
+
+    /** This method returns value of the current label.
+	@return An integer containing value of the label. */
+    public int returnLabelValue(){} 
+
+
     public boolean returnDefiningDS(){} // samoin
     public boolean returnDefiningDC(){} //samoin
+
+    /** This method returns the value of DC or DS.
+	@return An integer containing value of DC or DS.
+    */
     public int returnValue(){}
+
+    /** This method returns true if field finalFinal is set.
+	@return Boolean.
+    */
     public boolean returnFinalPhase(){}
+
+    /** This method returns initial SP and FP pointers in an interger array
+	where first value is SP and second is FP.
+	@return An integer array.
+    */
     public int[] returnInitPointers(){}
 
 
