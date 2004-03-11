2ae545cf175f0df4909d64346426a79a1be5b1ba
Sampo Yrjänäinen <szyrjana@cs.helsinki.fi>
2004-03-11 20:25:51 +0000

    korjattu kääntyväksi

--- CompileConstants.java
+++ CompileConstants.java
@@ -1,16 +1,18 @@
 package fi.hu.cs.titokone;
 
+import java.util.Hashtable;
+
 /** This class contains the bulk data needed to translate commands to 
     opcodes and vice versa. 
     TODO: Anything else needed? */
 public class CompileConstants {
-    private HashTable opcodes;
-    private HashTable commands;
+    private Hashtable opcodes;
+    private Hashtable commands;
 
     private static final Object[][] data = {
 	{"NOP", new Integer(0)},
 	{"STORE", new Integer(1)} // etc.
-    }
+    };
 
     public CompileConstants() { // Set up the hashtables.
     }
