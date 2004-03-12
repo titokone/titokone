55c1628819476fbd1096570aea9a5df55db676ba
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-03-12 20:15:08 +0000

    Luuranko animaattorille.

---
+++ Animator.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+/** This class takes care of the animation window. It digs the 
+    needed information from a RunInfo instance. 
+
+    <p>(This skeleton does not yet describe any private methods or 
+    fields, nor does it import everything possibly needed in the
+    future.)</p> */
+public class Animator extends JFrame {
+
+    /** This method produces an animation of a command based on 
+	the information in the RunInfo parameter.
+	@param info A RunInfo to base the animation on. */
+    public void animate(RunInfo info) {}
+}
