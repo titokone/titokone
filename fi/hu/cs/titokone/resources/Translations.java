5e0fc8d48bc2c43d8ac9c8ba2d1d7fdf28946aec
Sini Ruohomaa <ruohomaa@cs.helsinki.fi>
2004-03-19 17:41:32 +0000

    Applicationin stringit kaivettu.

--- fi/hu/cs/titokone/resources/Translations.java
+++ fi/hu/cs/titokone/resources/Translations.java
@@ -9,7 +9,8 @@ import java.util.ListResourceBundle;
     put between ? and :, eg. "?Menu:File" would mean "translate the 
     word 'File' as if you would see it on the common menu name" as 
     opposed to "?Utility item:File", which might mean "translate the 
-    word 'File' as you would if it meant a nail file". */
+    word 'File' as you would if it meant a nail file". 
+    Use */
 class Translations extends ListResourceBundle {
    public Object[][] getContents() { 
      return contents;
@@ -17,8 +18,39 @@ class Translations extends ListResourceBundle {
 
   protected static final Object[][] contents = {
   // Localize below, pairs of key-value (what key is in your language)...
-    { "Menu:File", "File" },
-    { "Menu:Options", "Options" }
+      // Class: Application.
+      // General messages: (none)
+      // Exception messages:
+      { "No more keyboard data stored on application." , 
+        "No more keyboard data stored on application." },
+      { "No more stdin data stored on application.", 
+	"No more stdin data stored on application." },
+      {	"Keyboard input string \"{0}\" invalid, should be eg. \n-separated " +
+	"list of integers." , 
+	"Keyboard input string \"{0}\" invalid, should be eg. \n-separated " +
+	"list of integers." },
+      { "Stdin input string \"{0}\" invalid, should be eg. \n-separated " +
+	"list of integers.", 
+	"Stdin input string \"{0}\" invalid, should be eg. \n-separated " +
+	"list of integers." },
+      { , },
+      { , },
+      // Log messages:
+      { "Application has no more keyboard data, read: {0}, buffer length " +
+	"{1}.", 
+	"Application has no more keyboard data, read: {0}, buffer length " +
+	"{1}." },
+      { "Application has no more stdin data, read: {0}, buffer length {1}." , 
+	"Application has no more stdin data, read: {0}, buffer length {1}." },
+      { "Accepted \"{0}\" as keyboard input, tokens found: {1}." , 
+	"Accepted \"{0}\" as keyboard input, tokens found: {1}." },
+      { "Accepted \"{0}\" as stdin input, tokens found: {1}.", 
+	"Accepted \"{0}\" as stdin input, tokens found: {1}." }      
+      // Class: 
+      // General messages:
+      // Exception messages:
+      // Log messages:
+      // ...
   // Localizable bit ends.
   }
 }
