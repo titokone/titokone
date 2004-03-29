4a93437318b45843e6cf69384e697a8f2f0d4aa4
Olli Alm <alm@cs.helsinki.fi>
2004-03-29 15:57:33 +0000

    *** empty log message ***

--- fi/hu/cs/titokone/DebugInfo.java
+++ fi/hu/cs/titokone/DebugInfo.java
@@ -11,23 +11,23 @@ public class DebugInfo{
     public DebugInfo(){}
 
     /** This field contains message to the GUI's statusbar.*/
-    private String statusMessage;
+    private Message statusMessage;
     
     /** This field contains comments made by debugger. */
-    private String comments;
+    private Message comments;
 
     /** This method sets statusMessage field to given string.
 	@param statusMessage String containing the message.
     */
     public void setStatusMessge(String statusMessage){ 
-	this.statusMessage = statusMessage;
+	this.statusMessage = new Message(statusMessage);
     }
 
     /** This method sets comments field to given string.
 	@param comments String containing comments.
     */
     public void setComments(String comments){
-	this.comments = comments;
+	this.comments = new Message(comments);
     }
 
 
@@ -35,13 +35,13 @@ public class DebugInfo{
 	@return String containing the message.
     */
     public String getStatusMessage(){
-	return this.statusMessage;
+	return this.statusMessage.toString();
     }
 
     /** This method returns comments.
 	@return String containing comments..
     */
     public String getComments(){
-	return this.comments;
+	return this.comments.toString();
     }
 }
