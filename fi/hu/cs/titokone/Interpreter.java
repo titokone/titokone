6abeb3d5db0cea9d05d2d20980e9d1f13aaf616d
Seppo Hätönen <shatonen@cs.helsinki.fi>
2004-04-01 12:16:59 +0000

    korjattu

--- fi/hu/cs/titokone/Interpreter.java
+++ fi/hu/cs/titokone/Interpreter.java
@@ -1,6 +1,8 @@
 package fi.hu.cs.titokone;
 
 /** This class contains information common to various interpreters. */
+//TODO tarkistuksia..
+
 public class Interpreter {
     /** This field specifies the opcode field length in number of bits. */
     public static final int opcodeLength = 8; 
@@ -16,7 +18,11 @@ public class Interpreter {
     public static final Integer SP_ONLY = new Integer(3);
     public static final Integer ADDR = new Integer(4); // Addr only.
     public static final Integer FULL = new Integer(5);
-
+    public static final Integer FULL_LESS_FETCHES = new Integer(6);
+    public static final Integer REG_DEVICE = new Integer(7);
+    public static final Integer ADDR_LESS_FETCH = new Integer(8);
+    public static final Integer SVC = new Integer(9);
+  
     /** This field contains a two-dimensional array of translations 
 	between opcodes as integers, as symbolic command names and the
 	variety of parameters they accept (nothing, one register (usually 
@@ -26,10 +32,10 @@ public class Interpreter {
     protected static final Object[][] commandData = {
 	{"NOP", new Integer(0), NONE},
         {"STORE", new Integer(1), FULL},
-        {"LOAD", new Integer(2), FULL},
-        {"IN", new Integer(3), FULL},	//I guess you could have KBD stored in
+        {"LOAD", new Integer(2), FULL_LESS_FETCHES},
+        {"IN", new Integer(3), REG_DEVICE},	//I guess you could have KBD stored in
 	                                //some weird way
-        {"OUT", new Integer(4), FULL},
+        {"OUT", new Integer(4), REG_DEVICE},
         {"ADD", new Integer(17), FULL},
         {"SUB", new Integer(18), FULL},
         {"MUL", new Integer(19), FULL},
@@ -43,7 +49,7 @@ public class Interpreter {
         {"SHR", new Integer(26), FULL},
 	{"SHRA", new Integer(27), FULL},	//same int as NOT!,
         {"COMP", new Integer(31), FULL},
-        {"JUMP", new Integer(32), ADDR},	//Jump has only one param. 
+        {"JUMP", new Integer(32), ADDR_LESS_FETCH},	//Jump has only one param. 
 	                                        //Jump Address
         {"JNEG", new Integer(33), FULL},	//JNEG Rj, Address
         {"JZER", new Integer(34), FULL},
@@ -51,19 +57,19 @@ public class Interpreter {
         {"JNNEG", new Integer(36), FULL},
         {"JNZER", new Integer(37), FULL},
         {"JNPOS", new Integer(38), FULL},
-        {"JLES", new Integer(39), FULL},
-        {"JEQU", new Integer(40), FULL},
-        {"JGRE", new Integer(41), FULL},
-        {"JNLES", new Integer(42), FULL},
-        {"JNEQU", new Integer(43), FULL},
-        {"JNGRE", new Integer(44), FULL},
+        {"JLES", new Integer(39), ADDR_LESS_FETCH},
+        {"JEQU", new Integer(40), ADDR_LESS_FETCH},
+        {"JGRE", new Integer(41), ADDR_LESS_FETCH},
+        {"JNLES", new Integer(42), ADDR_LESS_FETCH},
+        {"JNEQU", new Integer(43), ADDR_LESS_FETCH},
+        {"JNGRE", new Integer(44), ADDR_LESS_FETCH},
         {"CALL", new Integer(49), FULL},
         {"EXIT", new Integer(50), FULL},
         {"PUSH", new Integer(51), FULL},
         {"POP",  new Integer(52), SP_REG},	
-        {"PUSHR", new Integer(53), NONE},	//not sure, would be odd tho
-        {"POPR",  new Integer(54), NONE},	//same with this one.
-        {"SVC", new Integer(112), FULL}
+        {"PUSHR", new Integer(53), SP_ONLY},	//not sure, would be odd tho
+        {"POPR",  new Integer(54), SP_ONLY},	//same with this one.
+        {"SVC", new Integer(112), SVC}
     };
     
     /** This field contains a two-dimensional array of translations
@@ -85,9 +91,9 @@ public class Interpreter {
         {"R3", new Integer(3)},
         {"R4", new Integer(4)},
         {"R5", new Integer(5)},
-        {"R6", new Integer(6)},
-        {"R7", new Integer(7)},
         {"SP", new Integer(6)},
         {"FP", new Integer(7)},
-    };
+        {"R6", new Integer(6)},
+        {"R7", new Integer(7)},
+   };
 }
