52e47705b15c9fb7811c98262b769fc442462fde
Sampo Yrjänäinen <szyrjana@cs.helsinki.fi>
2004-04-04 01:14:21 +0000

    nimeämistä yhtenäistetty

--- fi/hu/cs/titokone/SymbolTable.java
+++ fi/hu/cs/titokone/SymbolTable.java
@@ -4,20 +4,15 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.Iterator;
 
-// Sini 26.3.: 
-// void addDefinition(String, String), String getDefinition(String),
-// String[] getAllDefinitions() 
-// and a separate hashmap for them added. The actual 
-// implementation does not matter, but the methods are needed. If you
-// change their names, corresponding changes must be made in Control.
-
 
 /** This class represents the symbol table of a TTK-91 program. It contains 
-    only symbols with defined values. */
+    symbols with defined values. It also contains DEF-definitions, which are
+    like symbols except the value is String. Symbols and DEFs are in separated
+    maps. */
 public class SymbolTable {
     /** This field contains the SymbolTable's symbols. */
     private HashMap symbols = new HashMap();
-    /** This field contains the SymbolTable's definitions. (Sini 23.3.) */
+    /** This field contains the SymbolTable's definitions. */
     private HashMap definitions = new HashMap();
 
     /** This function returns the integer value that corresponds to a 
@@ -26,7 +21,7 @@ public class SymbolTable {
     @return Integer value that corresponds to the symbol.
     @throws InvalidSymbolException If there is no such
     symbol in the SymbolTable. */
-    public int getSymbolValue(String symbolName) 
+    public int getSymbol(String symbolName) 
         throws InvalidSymbolException {
         if (!symbols.containsKey (symbolName))
             throw new InvalidSymbolException ("Symbol " + symbolName + " not found.");
@@ -49,10 +44,10 @@ public class SymbolTable {
     }
 
     /** This method adds a new symbol to the symbol table. If the
-    symbol was already definedm this changes its value.
+    symbol was already defined this changes its value.
     @param symbolName Name of the symbol.
     @param symbolValue Integer value of the symbol. */
-    public void defineSymbol(String symbolName, int symbolValue) {
+    public void addSymbol(String symbolName, int symbolValue) {
         if (symbolName == null) throw new IllegalArgumentException ("symbolName: null");
         symbols.put (symbolName, new Integer(symbolValue));
     }
@@ -89,13 +84,13 @@ public class SymbolTable {
     @return An Array that contains all the definition keys with values 
     to them. */
     public String[] getAllDefinitions() {
-	Set definitionSet = definitions.keySet();
-	String[] defs = new String[definitionSet.size()];
-	Iterator defIt = definitionSet.iterator();
-	int i=0;
-	while (defIt.hasNext())
-	    defs[i++] = (String) defIt.next();
-	return defs;
+	    Set definitionSet = definitions.keySet();
+	    String[] defs = new String[definitionSet.size()];
+	    Iterator defIt = definitionSet.iterator();
+	    int i=0;
+	    while (defIt.hasNext())
+    	    defs[i++] = (String) defIt.next();
+    	return defs;
     }
 
     /** This method returns the symbol table as a hashmap. The 
