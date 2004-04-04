f9d2e0e059110335c2ecaa31fda891d66648263f
Sampo Yrjänäinen <szyrjana@cs.helsinki.fi>
2004-04-04 23:28:13 +0000

    Muutettu RuntimeExceptioniksi

--- fi/hu/cs/titokone/InvalidSymbolException.java
+++ fi/hu/cs/titokone/InvalidSymbolException.java
@@ -2,7 +2,7 @@ package fi.hu.cs.titokone;
 
 /** This class represents a situation where a symboltable has been
     queried for a symbol it does not contain. */
-public class InvalidSymbolException extends Exception {
+public class InvalidSymbolException extends RuntimeException {
     /** This constructor sets up an instance of the class. 
 	@param message The message to describe the problem more
 	verbosely, for the user's eyes. */
