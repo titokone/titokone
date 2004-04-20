d857b8c1310f8daac1a8e330c8c04d3fba0fcc7a
Antti Takalahti <takalaht@cs.helsinki.fi>
2004-04-20 12:29:58 +0000

    typo

--- fi/hu/cs/titokone/resources/Translations.java
+++ fi/hu/cs/titokone/resources/Translations.java
@@ -209,7 +209,7 @@ public class Translations extends ListResourceBundle {
       // Class: RunDebugger, comments
       {"{0}{1} Indexing {2}, direct.", null},
       {"{0}{1} Indexing {2}, direct addressing.", null},
-      {"{0}{1} Indexing {2}, indirect addressing"., null},
+      {"{0}{1} Indexing {2}, indirect addressing.", null},
       
       // Comment with value (=KBD, =CRT =STDIN =STDOUT)
       {"{0}{1} Indexing {2}, direct, value {3}.", null},
