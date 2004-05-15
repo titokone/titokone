a5cb928ce2d57eade29c8d1499ea25b71f4d1a3b
Olli Alm <alm@cs.helsinki.fi>
2004-05-15 15:40:42 +0000

    *** empty log message ***

--- fi/hu/cs/titokone/AssariUI.java
+++ fi/hu/cs/titokone/AssariUI.java
@@ -17,65 +17,34 @@ String output;
 
 public void doGet(HttpServletRequest req, HttpServletResponse res)
    throws ServletException, IOException {
-   
-   res.setContentType("text/html");
-   ServletOutputStream out= res.getOutputStream();        
-   out.println("<html><head><title>Assari User Interface</title></head>");
-   out.println("<body><form method=\"POST\" action=\"AssariUI\" method=\"get\">");
-   out.println("<table border=\"0\" width=\"100%\">");
-   out.println("<tr><td width=\"20%\">TTK-91 code</td><td width=\"20%\"></td>");
-   out.println("<td width=\"20%\"></td><td width=\"20%\"></td><td width=\"20%\"></td></tr>");
-   out.println("<tr><td width=\"20%\" rowspan=\"4\"><textarea rows=\"11\" name=\"code\" cols=\"33\">");
-   
-   //tähän seuraavaan printn-kenttään tulee sama lähdekoodi
-   out.println(req.getParameter("code") + "</textarea></td><td width=\"20%\"></td><td width=\"20%\" rowspan=\"4\"></td>");
-   
-   out.println("<td width=\"20%\" rowspan=\"4\"></td><td width=\"20%\" rowspan=\"4\"></td></tr><tr>");  
-   out.println("<td width=\"20%\"><input type=\"submit\" value=\"Käännä ohjelma\" name=\"B1\"></td>");
-   out.println("</tr><tr><td width=\"20%\"><input type=\"submit\" value=\"Compile and run\" name=\"B2\"></td>");
-   out.println("</tr><tr><td width=\"20%\"><input type=\"submit\" value=\"Run\" name=\"B3\">");
-   out.println("<input type=\"text\" name=\"lines\" size=\"3\" value=\"" +req.getParameter("lines") + "\"> lines </td></tr>");
-   out.println("<tr><td width=\"20%\">KBD input</td><td width=\"20%\"></td>");
-   out.println("<td width=\"20%\">Program output</td><td width=\"20%\"></td>");
-   out.println("<td width=\"20%\"></td></tr><tr>");
 
-String error = "";
-String kbd = "";
-kbd = req.getParameter("kbd");
-
-
-//tähän samat myös
-   out.println("<td width=\"20%\"><textarea rows=\"8\" name=\"kbd\" cols=\"33\">"+ kbd + "</textarea></td>");
-   out.println("<td width=\"20%\" rowspan=\"3\"></td>");
-   out.println("<td width=\"20%\" rowspan=\"3\"><textarea rows=\"17\" name=\"output\" cols=\"33\">");
-
-source = new Source(req.getParameter("code"));
-
-core = new Control(null, null);
-
-boolean runningException = false;
-
-String kbdInput = "";
-output = "";
-String status = "";
-
+   String code = req.getParameter("code");
+   String noOflines = req.getParameter("lines");
+   String kbd = req.getParameter("kbd");
+   String stdin = req.getParameter("stdin");
+   String error = "";
+   output = "";
+   String status = "";   
+      
+   source = new Source(req.getParameter("code"));
+   core = new Control(null, null);
+   boolean runningException = false;
+            
+   //COMPILE
    if(req.getParameter("B1")!=null) {
       try {
 	    app = core.compile(source);
-	    app.setKbd(req.getParameter("kbd"));
-	    app.setStdIn(req.getParameter("stdin"));
 	    status = "Compilation ok!";
       } catch(TTK91Exception e) {
 	   runningException = false;	
 	   error = e.toString();   
-      } catch(IllegalArgumentException e) {
-	   runningException = false;
- 	   error = e.toString();
-      }
+	
+      } //COMPILE AND RUN
     } else if(req.getParameter("B2")!=null) {
       try {
          app = core.compile(source);
-	     app.setKbd(req.getParameter("kbd"));
+	 runningException = true;
+         app.setKbd(req.getParameter("kbd"));
          app.setStdIn(req.getParameter("stdin"));
          
          core.run(app, 0);
@@ -84,66 +53,86 @@ String status = "";
          status = "Execution ok!";
          output = createOutput();
       } catch(TTK91Exception e) {
-          runningException=true;
 	      error = e.toString();
       } catch(IllegalArgumentException e) {
 	    runningException = false;
             error = e.toString();
       }
-   }
+   } // RUN
    else if(req.getParameter("B3")!=null) {
 	   try {
          app = core.compile(source);
+	 runningException=true;
 	 app.setKbd(req.getParameter("kbd"));
          app.setStdIn(req.getParameter("stdin"));
          
-         if(req.getParameter("lines").equals("") || req.getParameter("lines").matches("[^0123456789]"))
-	        error = "Invalid number of lines.";
-         else {
-          	 int lines = Integer.parseInt(req.getParameter("lines"));
+          	int lines = Integer.parseInt(req.getParameter("lines"));
          	core.run(app, lines);
          	memory = core.getMemory();
 		cpu = core.getCpu();
          	status = lines + " lines executed.";
          	output = createOutput();
-		}
         } catch(TTK91Exception e) {
-          runningException=true;
 	      error = e.toString();
       } catch(IllegalArgumentException e) {
-	     runningException = true;
              error = e.toString();
 	}
- 
-
    }
-       
-   
-   //tämä kenttä on keskeinen: tulosteet painetun napin mukaan
-   out.println(output + "</textarea></td>");
-   out.println("<td width=\"20%\" rowspan=\"3\"></td><td width=\"20%\" rowspan=\"3\"></td>");
-   out.println("</tr><tr><td width=\"20%\">STDIN input</td></tr><tr>");
-
-   //tarkistetaan parametri
-
-   String stdin = "1";
-   if(!req.getParameter("stdin").equals(""))
-     stdin = req.getParameter("stdin");
-
-
-
-   out.println("<td width=\"20%\"><textarea rows=\"7\" name=\"stdin\" cols=\"33\">" + stdin + "</textarea></td>");
-   out.println("</tr></table><p>&nbsp;</p><p>&nbsp;</p>");
+   String printstate;
    if(error.equals(""))
-      out.println("<font face=\"sans-serif\" size=4 color=\"blue\">" + status + "</font>");
+      printstate = "<p><font face=\"sans-serif\" size=4 color=\"blue\">" + status + "</font></p>";
    else {
        if(runningException)
            error = "Execution failed:" + error.substring(error.indexOf(':')+1);
        else
-	    error = error.substring(error.indexOf(':')+1);
-       out.println("<font face=\"sans-serif\" size=4 color=\"red\">" + error + "</font>");
+	   error = error.substring(error.indexOf(':')+1);
+       printstate = "<p><font face=\"sans-serif\" size=4 color=\"red\">" + error + "</font></p>";
    }
-       out.println("</body></html>");
+               
+   res.setContentType("text/html");
+   ServletOutputStream out= res.getOutputStream();        
+   out.println("<html><head><title>Assari User Interface</title></head>");
+   out.println("<body bgcolor=\"#CCCCCC\">");
+   
+   out.println(printstate);
+   
+   out.println("<form method=\"POST\" action=\"AssariUI\" method=\"get\"><table border=\"0\" width=\"100%\">");
+   out.println("<tr><td width=\"20%\">TTK-91 code</td><td width=\"20%\"></td>");
+   out.println("<td width=\"20%\"></td><td width=\"20%\"></td><td width=\"20%\"></td></tr>");
+   out.println("<tr><td width=\"20%\" rowspan=\"4\"><textarea rows=\"11\" name=\"code\" cols=\"33\">");
+   
+   out.println(code);
+   
+   out.println("</textarea></td><td width=\"20%\"></td><td width=\"20%\" rowspan=\"4\"></td>");
+   out.println("<td width=\"20%\" rowspan=\"4\"></td><td width=\"20%\" rowspan=\"4\"></td></tr><tr>");  
+   out.println("<td width=\"20%\"><input type=\"submit\" value=\"Compile\" name=\"B1\"></td>");
+   out.println("</tr><tr><td width=\"20%\"><input type=\"submit\" value=\"Compile and run\" name=\"B2\"></td>");
+   out.println("</tr><tr><td width=\"20%\"><input type=\"submit\" value=\"Compile and then run\" name=\"B3\">");
+   out.println("<input type=\"text\" name=\"lines\" size=\"3\" value=\"");
+   
+   out.println(noOflines);
+   
+   out.println("\"> lines </td></tr>");
+   out.println("<tr><td width=\"20%\">KBD input</td><td width=\"20%\"></td>");
+   out.println("<td width=\"20%\">Program output</td><td width=\"20%\"></td>");
+   out.println("<td width=\"20%\"></td></tr><tr>");
+   out.println("<td width=\"20%\"><textarea rows=\"8\" name=\"kbd\" cols=\"33\">");
+   
+   out.println(kbd);
+   
+   out.println("</textarea></td>");
+   out.println("<td width=\"20%\" rowspan=\"3\"></td>");
+   out.println("<td width=\"20%\" rowspan=\"3\"><textarea rows=\"17\" name=\"output\" cols=\"33\">");
+   out.println(output + "</textarea></td>");
+   out.println("<td width=\"20%\" rowspan=\"3\"></td><td width=\"20%\" rowspan=\"3\"></td>");
+   out.println("</tr><tr><td width=\"20%\">STDIN input</td></tr><tr>");
+   out.println("<td width=\"20%\"><textarea rows=\"7\" name=\"stdin\" cols=\"33\">");
+   
+   out.println(stdin);
+   
+   out.println("</textarea></td></tr></table></body></html>");
+   
+       
 
 
 }
@@ -151,6 +140,7 @@ public void doPost(HttpServletRequest req, HttpServletResponse res)
    throws ServletException, IOException {
     // do the same as doget
    doGet(req,res);
+
 }
 
 private String createOutput() {
@@ -163,21 +153,14 @@ private String createOutput() {
 	            "\nSP: " + cpu.getValueOf(TTK91Cpu.REG_R6) +
 	            "\nFP: " + cpu.getValueOf(TTK91Cpu.REG_R7) +
 	            "\n---------------------" +
-	            "\nCRT: " + app.readCrt() +
+	            "\nCRT:\n" + app.readCrt() +
 	            "\n---------------------" + 
-                    "\nSTDOUT: " + app.readStdOut() +
+                    "\nSTDOUT:\n" + app.readStdOut() +
                     "\n---------------------" +
-		    "\nMemorylines 0-9: " + 
-                    "\n0: " + memory.getValue(0) +
-		    "\n1: " + memory.getValue(1) +
-                    "\n2: " + memory.getValue(2) +
-                    "\n3: " + memory.getValue(3) +
-                    "\n4: " + memory.getValue(4) +
-                    "\n5: " + memory.getValue(5) +
-                    "\n6: " + memory.getValue(6) +
-                    "\n7: " + memory.getValue(7) +
-                    "\n8: " + memory.getValue(8) +
-                    "\n9: " + memory.getValue(9);
+		    "\nMemorylines 0-49: ";
+        //muistirivien täyttäminen output-ikkunaan, oletus: 50 riviä
+	for(int i=0; i < 50; i++) 
+                    output += "\n"+i+": " + memory.getValue(i);
 	 return output;
 	
 }
