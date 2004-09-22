ffea6e2eb423e811a59447c8ba31b079a955c22c
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-09-22 10:48:06 +0000

    Ja loputkin puuttuneet titokoneen sorsien tiedostot cvs:ään. Nämä nyt varmaan olivat aika turhia bonareita, mutta laitetaan kuitenkin. / Lauri

---
+++ titokone2.bat
@@ -0,0 +1,2 @@
+@echo off
+java.exe fi.hu.cs.titokone.Titokone
---
+++ titokone2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# You can use this script to run the Titokone after opening it from the 
+# jar package.
+java -classpath=. fi.hu.cs.titokone.Titokone
