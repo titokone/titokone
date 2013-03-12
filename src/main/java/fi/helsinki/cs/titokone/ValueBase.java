ededdee5094fda32adbd7fb5d336f772cfb1a312
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-03-12 14:43:45 +0200

    No need to check value if hex notation is used

--- src/main/java/fi/helsinki/cs/titokone/ValueBase.java
+++ src/main/java/fi/helsinki/cs/titokone/ValueBase.java
@@ -31,7 +31,7 @@ public enum ValueBase {
 	public String getName() { return name; }
 
 	public String toString(int value) {
-		if (value < 0 && this == ValueBase.HEX)
+		if (this == ValueBase.HEX)
 			return prefix + Long.toString(value & 0xFFFFFFFFL, base) + suffix;
 		return prefix + Integer.toString(value, base) + suffix;
 	}
